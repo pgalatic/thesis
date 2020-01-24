73f658c538de9ceabea4239123b1aad4cd4a06bb
Paul Galatic <pgalatic@gmail.com>
2020-01-24 09:12:37 -0500

    Update required Cuda/Cudnn packages

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 # author: Paul Galatic
-# Script assumes that .deb package files for cuda/cudnn are already present
+# Script assumes that .deb package files for cuda/cudnn are already present 
+#   and in the home directory
+
+cd ~/
 
 # Update package lists
 sudo apt-get -y update
@@ -16,20 +19,18 @@ sudo apt-get install cuda-7-5
 export PATH=/usr/local/cuda/bin${PATH:+:${PATH}}
 
 # Install CUDNN v5.0
-tar -xzvf cudnn-8.0-linux-x64-v5.1.tgz
+tar -xzvf cudnn-7.5-linux-x64-v5.0-ga.tgz
 sudo cp cuda/include/cudnn.h /usr/local/cuda/include
 sudo cp cuda/lib64/libcudnn* /usr/local/cuda/lib64
 sudo chmod a+r /usr/local/cuda/include/cudnn.h /usr/local/cuda/lib64/libcudnn*
 export CUDNN_PATH="/usr/local/cuda/lib64/libcudnn.so.5"
 
 # Install the latest Torch
+export TORCH_NVCC_FLAGS="-D__CUDA_NO_HALF_OPERATORS__"
 git clone https://github.com/torch/distro.git ~/torch --recursive
 cd ~/torch; bash install-deps;
-export TORCH_NVCC_FLAGS="-D__CUDA_NO_HALF_OPERATORS__"
 ./install.sh
 ./update.sh
-
-cd ~/
 
 # Add Torch to PATH
 source ~/.bashrc
@@ -44,9 +45,15 @@ luarocks install cutorch
 luarocks install cunn
 luarocks install cudnn
 
+# This package is included with the repo for reasons of compatibility and is 
+# compiled manually
 cd ~/thesis/core/stnbhw
 luarocks make stnbdhw-scm-1.rockspec
-cd ~/
+
+# Download models
+cd ~/thesis/styles
+bash _download_models.sh
+cd ~
 
 # Clean installation
 sudo apt-get -y update
