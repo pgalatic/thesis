f3de465d032932be4e376bf8ac47e2c6d3a2c3f1
Paul Galatic <pgalatic@gmail.com>
2020-01-24 12:42:46 -0500

    Update versioning

--- install.sh
+++ install.sh
@@ -8,24 +8,27 @@ sudo apt-get -y update
 # Install FFMPEG
 sudo apt-get -y install ffmpeg
 
-# Install CUDA v8.0
-sudo dpkg -i cuda-repo-ubuntu1604_8.0.44-1_amd64.deb
-sudo apt-get update
-sudo apt-get install cuda-8-0
-# Let Cmake find CUDA
-export PATH=/usr/local/cuda/bin${PATH:+:${PATH}}
+# Install CUDA v9.2
+sudo apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/7fa2af80.pub
+sudo dpkg -i cuda-repo-ubuntu1604_9.2.148-1_amd64.deb
+sudo apt-get -y update
+sudo apt-get -y install cuda-9-2
 
-# Install CUDNN v5.0 (for CUDA v8.0)
-tar -xzvf cudnn-8.0-linux-x64-v5.0-ga.tgz
+# Install CUDNN v7.6.5 (for CUDA v9.2)
+tar -xzvf cudnn-9.2-linux-x64-v7.6.5.32.tgz
 sudo cp cuda/include/cudnn.h /usr/local/cuda/include
 sudo cp cuda/lib64/libcudnn* /usr/local/cuda/lib64
 sudo chmod a+r /usr/local/cuda/include/cudnn.h /usr/local/cuda/lib64/libcudnn*
-export CUDNN_PATH="/usr/local/cuda/lib64/libcudnn.so.5"
+
+# Let Cmake find CUDA/CUDNN
+export PATH=/usr/local/cuda${PATH:+:${PATH}}
+export CUDNN_PATH="/usr/local/cuda/lib64/libcudnn.so.7"
+export TORCH_NVCC_FLAGS="-D__CUDA_NO_HALF_OPERATORS__"
 
 # Install the latest Torch
-export TORCH_NVCC_FLAGS="-D__CUDA_NO_HALF_OPERATORS__"
 git clone https://github.com/torch/distro.git ~/torch --recursive
-cd ~/torch; bash install-deps;
+cd ~/torch
+bash install-deps
 bash install.sh
 bash update.sh
 
@@ -52,7 +55,16 @@ cd ~/thesis/styles
 bash _download_models.sh
 cd ~
 
+# Update Torch bindings
+git clone https://github.com/soumith/cudnn.torch -b R7
+cd cudnn.torch
+luarocks make cudnn-scm-1.rockspec
+
+# Let Cmake find CUDA/CUDNN
+export PATH=/usr/local/cuda${PATH:+:${PATH}}
+export CUDNN_PATH="/usr/local/cuda/lib64/libcudnn.so.7"
+export TORCH_NVCC_FLAGS="-D__CUDA_NO_HALF_OPERATORS__"
+
 # Clean installation
 sudo apt-get -y update
-sudo apt-get -y upgrade
 sudo apt-get -y autoremove
